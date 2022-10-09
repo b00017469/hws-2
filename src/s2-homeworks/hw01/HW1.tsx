c4106fe7797400ad00b11daaefc42df399df3082
Pavel Morozuk <morozuki@mail.ru>
2022-10-09 22:21:51 +0300

    hw1

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,7 +14,17 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number
+    user: {
+        avatar: string
+        name: string
+    }
+    message: {
+        text: string
+        time: string
+    }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
@@ -26,7 +36,7 @@ export const message0: MessageType = {
     message: {
         text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
         time: '22:00', // можно менять
-    },
+    }
 }
 export const friendMessage0: MessageType = {
     id: 100,
